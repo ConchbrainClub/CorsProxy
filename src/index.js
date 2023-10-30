31fd276912a98a6e4e4ad44639238aec77fd5f05
LLLXY <lixinyangemil@outlook.com>
2023-10-30 23:21:10 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,44 +7,35 @@ async function handleRequest(request) {
     let para = new URL(request.url).search.replace("?", "")
     let url = decodeURI(para)
 
+		if (!url) {
+		    return new Response(`
+		        <div style="text-align:center margin-top: 100px">
+		            <p>Welcome to ConchBrain CORS Proxy</p>
+		            <p>Please read the <a href="https://www.conchbrain.club/#corsproxy" target="_blank">documentation</a></p>
+		        </div>
+		    `, {
+		        status: 200,
+		        headers: {
+		            "content-type": "text/html charset=utf-8"
+		        }
+		    })
+		}
+
     let body = await request.text()
+	
+    let response = await fetch(url, {
+				method: request.method,
+				body: body,
+				headers: request.headers
+		})
 
-    if (!body) {
-        body = undefined
-    }
-
-    if (isUrl(url)) {
-        let response = await fetch(url, {
-            method: request.method,
-            body: body,
-            headers: request.headers
-        })
-
-        return new Response(response.body, {
-            status: 200,
-            headers: {
-                "content-type": response.headers.get("content-type"),
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*",
-                "Access-Control-Allow-Headers": "*"
-            }
-        })
-    }
-
-    return new Response(`
-        <div style="text-align:center margin-top: 100px">
-            <p>Welcome to ConchBrain CORS Proxy</p>
-            <p>Please read the <a href="https://www.conchbrain.club/#corsproxy" target="_blank">documentation</a></p>
-        </div>
-    `, {
-        status: 200,
-        headers: {
-            "content-type": "text/html charset=utf-8"
-        }
-    })
-}
-
-function isUrl(href) {
-    var reg = /^((http|https):\/\/)?(([A-Za-z0-9]+-[A-Za-z0-9]+|[A-Za-z0-9]+)\.)+([A-Za-z]+)[/\?\:]?.*$/
-    return reg.test(href)
+		return new Response(response.body, {
+				status: 200,
+				headers: {
+						"content-type": response.headers.get("content-type"),
+						"Access-Control-Allow-Origin": "*",
+						"Access-Control-Allow-Methods": "*",
+						"Access-Control-Allow-Headers": "*"
+				}
+		})
 }
